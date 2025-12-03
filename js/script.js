4a8a062e670840e65410a9bae67610775e683f1d
unknown <sharmapandeyrohit@gmail.com>
2025-12-03 16:34:11 +0545

    dom Method getElementByID()

--- js/script.js
+++ js/script.js
@@ -9,5 +9,18 @@ function donkey() {
     var demo = document.getElementById("name");
 
     demo.innerHTML = person.name + " lives in " + person.city + " and is " + status + " today";
+
     demo.style.color = "red";
+    demo.style.backgroundColor = "yellow";
+    demo.style.padding = "10px";
+    demo.style.borderRadius = "10px";
+    demo.style.fontSize = "20px";
+    demo.style.fontWeight = "bold";
+    demo.style.textAlign = "center";
+    demo.style.margin = "10px";
+    demo.style.border = "1px solid black";
+    demo.style.boxShadow = "0 0 10px black";
+    demo.style.transition = "all 0.5s ease-in-out";
+    demo.style.cursor = "pointer";
+    demo.style.userSelect = "none";
 }
