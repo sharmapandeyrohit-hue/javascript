8ca3bc19dfb3616d4ce11132f44a508f98389000
unknown <sharmapandeyrohit@gmail.com>
2025-12-03 16:59:41 +0545

    dom Method getElementByClassName()

--- js/script.js
+++ js/script.js
@@ -6,21 +6,22 @@ const person = {
 function donkey() {
     const status = person.present ? "present" : "absent";
 
-    var demo = document.getElementById("name");
+    var demo = document.getElementsByClassName("name");
 
-    demo.innerHTML = person.name + " lives in " + person.city + " and is " + status + " today";
+    demo[2].innerHTML = person.name + " lives in " + person.city + " and is " + status + " today";
+    demo[2].style.color = "red";
+    demo[2].style.backgroundColor = "yellow";
+    demo[2].style.padding = "10px";
+    demo[2].style.borderRadius = "10px";
+    demo[2].style.fontSize = "20px";
+    demo[2].style.fontWeight = "bold";
+    demo[2].style.textAlign = "center";
+    demo[2].style.margin = "10px";
+    demo[2].style.border = "1px solid black";
+    demo[2].style.boxShadow = "0 0 10px black";
+    demo[2].style.transition = "all 0.5s ease-in-out";
+    demo[2].style.cursor = "pointer";
+    demo[2].style.userSelect = "none";
+    demo[2].style.outline = "none";
 
-    demo.style.color = "red";
-    demo.style.backgroundColor = "yellow";
-    demo.style.padding = "10px";
-    demo.style.borderRadius = "10px";
-    demo.style.fontSize = "20px";
-    demo.style.fontWeight = "bold";
-    demo.style.textAlign = "center";
-    demo.style.margin = "10px";
-    demo.style.border = "1px solid black";
-    demo.style.boxShadow = "0 0 10px black";
-    demo.style.transition = "all 0.5s ease-in-out";
-    demo.style.cursor = "pointer";
-    demo.style.userSelect = "none";
 }
