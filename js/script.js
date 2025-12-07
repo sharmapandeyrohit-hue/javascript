e6257877b73b870c6d3f8e115610fee14fef9e7c
unknown <sharmapandeyrohit@gmail.com>
2025-12-07 11:10:34 +0545

    added forlooop in getElementByClassName

--- js/script.js
+++ js/script.js
@@ -8,20 +8,21 @@ function donkey() {
 
     var demo = document.getElementsByClassName("name");
 
-    demo[2].innerHTML = person.name + " lives in " + person.city + " and is " + status + " today";
-    demo[2].style.color = "red";
-    demo[2].style.backgroundColor = "yellow";
-    demo[2].style.padding = "10px";
-    demo[2].style.borderRadius = "10px";
-    demo[2].style.fontSize = "20px";
-    demo[2].style.fontWeight = "bold";
-    demo[2].style.textAlign = "center";
-    demo[2].style.margin = "10px";
-    demo[2].style.border = "1px solid black";
-    demo[2].style.boxShadow = "0 0 10px black";
-    demo[2].style.transition = "all 0.5s ease-in-out";
-    demo[2].style.cursor = "pointer";
-    demo[2].style.userSelect = "none";
-    demo[2].style.outline = "none";
+    for (var i = 0; i < demo.length; i++) {
 
+        demo[i].innerHTML = person.name + " lives in " + person.city + " and is " + status + " today";
+        demo[i].style.color = "red";
+        demo[i].style.backgroundColor = "yellow";
+        demo[i].style.padding = "10px";
+        demo[i].style.borderRadius = "10px";
+        demo[i].style.fontSize = "20px";
+        demo[i].style.fontWeight = "bold";
+        demo[i].style.textAlign = "center";
+        demo[i].style.margin = "10px";
+        demo[i].style.border = "1px solid black";
+        demo[i].style.boxShadow = "0 0 10px black";
+        demo[i].style.transition = "all 0.5s ease-in-out";
+        demo[i].style.cursor = "pointer";
+        demo[i].style.userSelect = "none";
+    }
 }
